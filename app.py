e4be6b49458cb49d534bca6ae14b8cde636c0717
nathan <nwbryant@umich.edu>
2020-10-02 22:11:17 -0700

    Created basic routes for boards. Updated base.html with blocks. Protyping feed page. Made placeholder favicon

--- app.py
+++ app.py
@@ -9,3 +9,32 @@ def homepage():
     """Show homepage for lunaTutor"""
 
     return render_template('home.html')
+
+
+# GENERAL ROUTES FOR BOARDS
+@app.route('/q', methods=['GET', 'POST'])
+def feedpage():
+    """Show general feed for posted questions """
+
+    return render_template('/board/feed.html')
+
+
+@app.route('/q/biology', methods=['GET', 'POST'])
+def biologyBoard():
+    """Show question board for biology related questions """
+
+    return render_template('board.html')
+
+
+@app.route('/q/chemistry', methods=['GET', 'POST'])
+def chemistryBoard():
+    """Show question board for chemistry related questions  """
+
+    return render_template('board.html')
+
+
+@app.route('/q/physics', methods=['GET', 'POST'])
+def physicsBoard():
+    """Show question board for physics related questions  """
+
+    return render_template('board.html')
