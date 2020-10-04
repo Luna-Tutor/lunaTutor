a09ba23003ac726b005c0f1881d95d14b51257ae
nathan <nwbryant@umich.edu>
2020-10-04 10:48:50 -0700

    created question detail page, added front-end temporary functionality for upvoting

---
+++ static/js/questionDetail.js
@@ -0,0 +1,23 @@
+const favToggle = (event) => {
+  let eventClass = event.target.classList;
+  let qId = event.target.parentElement.dataset.questionId;
+  let $numUsers = $(`span[data-question-id=${qId}]`)[0];
+
+  console.log(event.target);
+
+  if (eventClass.contains("fav")) {
+    $numUsers.innerText = parseInt($numUsers.innerText) - 1;
+  } else {
+    if ($numUsers.innerText === "") {
+      $numUsers.innerText = "1";
+    } else {
+      $numUsers.innerText = parseInt($numUsers.innerText) + 1;
+    }
+  }
+
+  eventClass.toggle("fav");
+  eventClass.toggle("far");
+  eventClass.toggle("fas");
+};
+
+$(".upvote").on("click", favToggle);
