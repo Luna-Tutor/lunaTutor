67e1a0891bb50863bbc5728a2089f722957f5160
nathan <nwbryant@umich.edu>
2020-10-03 18:55:45 -0700

    changed subject list to list of tuples for feeding choices to select field

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 
-SUBJECT_LIST = ['Biology', 'Physics', 'Chemistry']
+SUBJECT_LIST = [('bio', 'Biology'), ('phy', 'Physics'), ('che', 'Chemistry')]
 
 
 class SignUpForm(FlaskForm):
@@ -10,52 +10,53 @@ class SignUpForm(FlaskForm):
 
     username = StringField('Name', validators=[
         DataRequired()
-        ])
+    ])
     email = StringField('Email', validators=[
         Email(message=('Not a valid email address.')),
         DataRequired()
-        ])
+    ])
     password = PasswordField('Password', validators=[
-        DataRequired(), 
+        DataRequired(),
         Length(min=6, message=('Password needs to be 6 or more characters!'))
-        ])
+    ])
     first_name = StringField('First Name', validators=[
         DataRequired()
-        ])
+    ])
     last_name = StringField('Last Name', validators=[
         DataRequired()
-        ])
+    ])
+
 
 class LoginForm(FlaskForm):
     """Login Form"""
 
     username = StringField('Name', validators=[
         DataRequired()
-        ])
+    ])
     password = PasswordField('Password', validators=[
-        DataRequired(), 
+        DataRequired(),
         Length(min=6, message=('Password needs to be 6 or more characters!'))
-        ])
+    ])
+
 
 class QuestionForm(FlaskForm):
     """Ask Question"""
 
     subject = SelectField('Subject', choices=SUBJECT_LIST, validators=[
-        DataRequired(),
-        choices = SUBJECT_LIST
-        ])
+        DataRequired()])
     title = StringField('Title', validators=[
         DataRequired()
-        ])
+    ])
     hashtag = StringField('Hashtag')
     content = StringField('Content', validators=[
         DataRequired(),
         Length(min=1, message=('Please fill in the details.'))
-        ])
+    ])
+
 
 class AnswerForm(FlaskForm):
     """Answer"""
 
     answer = TextAreaField('Answer', validators=[
-      DataRequired()
+        DataRequired()
     ])
